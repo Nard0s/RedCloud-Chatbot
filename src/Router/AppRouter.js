777bc5640c28a07964634deae7bc56c37ac60d83
nard0s <ninanteneh20@gmail.com>
2025-08-01 23:25:17 +0300

    login and sign up are linked

--- src/Router/AppRouter.js
+++ src/Router/AppRouter.js
@@ -5,13 +5,11 @@ import ChatRequest from '../Components/Chat/ChatRequest.js'
 import InputField from '../Components/Shared/InputField.js'
 import Chat from '../pages/Chat.js'
 import Button from '../Components/Shared/Button.js'
-// import LoginForm from '../Components/Auth/LoginForm.js'
-import RegisterForm from '../Components/Auth/RegisterForm.js'
+import LoginForm from '../Components/Auth/LoginForm.js'
 const AppRouter = () => {
   return (
     <div>
-      {/* <LoginForm/> */}
-      <RegisterForm/>
+      <LoginForm/>
      {/* <Chat/> */}
      {/* <Button/> */}
 
